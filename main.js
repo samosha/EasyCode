ed8c9fd35caef5b45e324f280dbc3ef54dc90546
Andrii S <andrii.dp.ua@gmail.com>
2017-11-07 11:20:50 +0200

    FSHN

---
+++ main.js
@@ -0,0 +1,61 @@
+
+var chislo = 25;
+var stroka = 'str4oka';
+
+console.log(chislo);
+console.log(stroka.length);
+console.log(stroka.toUpperCase());
+/*
+var z;
+outer:
+for(;;){
+
+for(z=0;z<10;z++){
+
+    if(z>4){
+        break outer;
+    }
+    alert('Hello, this is '+z);
+    
+}
+alert('last z = ' + z-1)
+}
+alert('laddddst z = ' + (z-1))
+*/
+
+
+/*switch*/
+/*
+var p = prompt('How old are you?')
+
+switch(p){
+    case '18':
+    alert('ura vam mogno');
+    break;
+    case '30':
+    alert('ura vam 30');
+    break;
+    default:
+    alert('vam i ne 18 i ne 30');
+}*/
+
+/*function*/
+/*
+function test(name,years){
+    var message;
+    if(years<1){
+        message=("Hello junior " + name);
+    }
+    else if(years>=1 && years<3){
+        message=("Hello middle "+name);
+    }
+    else{
+        message=('Hello senior ' + name);
+    }
+
+    alert(message);
+}
+
+test("Andrii", 3.5);
+test("Anna",0.5);
+test("Dimon",2.3);*/
